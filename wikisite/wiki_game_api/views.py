05f9c34189e98f6e882e3ba3558e1b19b2f7e638
James Hariady Widjaja <widjajajames01@gmail.com>
2023-05-01 02:23:19 -0400

    remove pycache

--- wikisite/wiki_game_api/views.py
+++ wikisite/wiki_game_api/views.py
@@ -5,13 +5,15 @@ import json
 
 # Create your views here.
 def game_request(request: HttpRequest):
+    print(request)
     wiki_start_link = request.GET.get("start")
     wiki_end_link = request.GET.get("end")
     if wiki_start_link is None or wiki_end_link is None:
+        print('a')
         return HttpResponseBadRequest("Argument is missing")
     else:
-        try:
+        # try:
             game_data = wiki_game(wiki_start_link, wiki_end_link)
             return JsonResponse(game_data)
-        except:
+        # except:
             return HttpResponseBadRequest("Something went wrong")
--- wikisite/wikisite/settings.py
+++ wikisite/wikisite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,10 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wiki_game_api.apps.WikiGameApiConfig',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'wikisite.urls'
@@ -126,9 +126,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
-""" SECURE_HSTS_SECONDS = 2,592,000
-SECURE_SSL_REDIRECT = True
+CORS_ALLOW_CREDENTIALS = False
+SECURE_HSTS_SECONDS = 2,592,000
+#SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE=True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SESSION_COOKIE_SECURE = True
-SECURE_HSTS_PRELOAD = True """
+SECURE_HSTS_PRELOAD = True
